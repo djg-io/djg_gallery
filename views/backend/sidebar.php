c0995ddf8a17fe0d91d6cbe7aac891d36b3b5c0d
djgprv <djgprv@gmail.com>
2013-10-14 21:57:55 +0200

    Documentation

--- views/backend/sidebar.php
+++ views/backend/sidebar.php
@@ -2,7 +2,7 @@
 <p class="button"><a href="<?php echo get_url('plugin/djg_gallery/thumbnails'); ?>"><img src="<?php echo URL_PUBLIC . 'wolf/plugins/djg_gallery/images/32_thumbnail.png'; ?>" align="middle" alt="" /><?php echo __('Thumbnails')?></a></p>
 <p class="button"><a href="<?php echo get_url('plugin/djg_gallery/settings'); ?>"><img src="<?php echo URL_PUBLIC . 'wolf/plugins/djg_gallery/images/32_settings.png'; ?>" align="middle" alt="" /><?php echo __('Settings')?></a></p>
 <p class="button"><a href="<?php echo get_url('plugin/djg_gallery/documentation'); ?>"><img src="<?php echo URL_PUBLIC . 'wolf/plugins/djg_gallery/images/32_documentation.png'; ?>" align="middle" alt="" /><?php echo __('Documentation')?></a></p>
-<p class="kreacjawww"><span><a href="http://kreacjawww.pl/">&copy; Micha≥ Uchnast - http://kreacjawww.pl/</a></span></p>
+<p class="kreacjawww"><span><a href="http://kreacjawww.pl/">&copy; Micha≈Ç Uchnast - http://kreacjawww.pl/</a></span></p>
 <div class="box">
     <h2><?php echo __('Dialog window'); ?></h2>
 	<div class="djg_dialog_window"></div>
